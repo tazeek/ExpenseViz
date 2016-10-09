b126a5e25e665ae18e6308b9ec382582a25db623
tazeek <tazeek.rakib@gmail.com>
2016-10-09 19:30:51 +0800

    'total' column added for weekly expense

--- example.py
+++ example.py
@@ -10,3 +10,6 @@ df.fillna(0)
 # Get all columns and remove 'week'
 columns = list(df)
 columns.remove('week')
+
+# Add all the rows (days) into 'total' column 
+df['total'] = df[columns].sum(axis=1)
