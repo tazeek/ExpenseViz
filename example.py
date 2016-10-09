213d29c58c15715ff1d03c52dc6c23546a84f175
tazeek <tazeek.rakib@gmail.com>
2016-10-09 19:20:58 +0800

    Dropping 'week' column and fill NaN values

--- example.py
+++ example.py
@@ -1,2 +1,12 @@
 import numpy as np
 import pandas as pd
+
+# Load the expense file
+df = pd.read_csv('expense.csv')
+
+# Fill remaining days, if any, with 0
+df.fillna(0)
+
+# Get all columns and remove 'week'
+columns = list(df)
+columns.remove('week')
