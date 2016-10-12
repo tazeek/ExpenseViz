cbcd486b4d8d1811a3f73de6f62e3ab74bfa49fe
tazeek <tazeek.rakib@gmail.com>
2016-10-13 00:26:32 +0800

    Loading in data from csv file. Requires an http server to be running

--- main.js
+++ main.js
@@ -1,17 +1,50 @@
-function intialize() {
-	
-	// For the graph
-	var margin = {top: 50, right: 30, bottom: 50, left: 50};
-	var width = 960 - margin.left - margin.right;
-	var height = 540 - margin.top - margin.bottom;
+// For the graph
+var margin = {top: 50, right: 30, bottom: 50, left: 50};
+var width = 960 - margin.left - margin.right;
+var height = 540 - margin.top - margin.bottom;
 
-	//For X-Axis functions
-	var xScale = d3.scale.linear().range([0, width]);
-	var xAxis = d3.svg.axis().scale(xScale).orient("bottom");
-	
-	//For Y-Axis functions 
-	var yScale = d3.scale.linear().range([height,0]);
-	var yAxis = d3.svg.axis().scale(yScale).orient("left");
-}
+//For X-Axis functions
+var xScale = d3.scale.linear().range([0, width]);
+var xAxis = d3.svg.axis().scale(xScale).orient("bottom").ticks(20);
 
-intialize()
+//For Y-Axis functions 
+var yScale = d3.scale.linear().range([height,0]);
+var yAxis = d3.svg.axis().scale(yScale).orient("left").ticks(20);
+
+//Add graph to the canvas of the webpage
+var svg = d3.select("body").append("svg")
+			.attr("width", width + margin.left + margin.right)
+			.attr("height", height + margin.top + margin.bottom)
+			.append("g")
+			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+				
+// Load the data 
+// NOTE: This is the function to be replaced by Python's Flask
+// NOTE: Refer to example.py for data processing
+d3.csv("expense.csv", function(error, data){
+	
+	//Get Keys and remove 'week' from the keys
+	keys = d3.keys(data[0]);
+	keys.shift();
+	
+	//Array to store the data 
+	total = []
+	
+	// Turn "String" into integers
+	data.forEach( function(d){
+		
+		// Calculate total
+		var sum = 0
+		
+		for(key in keys) { 
+			sum += +d[keys[key]];
+		}
+		
+		//Append
+		total.push(Math.round(sum))
+	});
+	
+	console.log(total)
+
+	return
+});
