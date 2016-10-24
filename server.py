e5e6f55a2b7e9757db204624d1079fdbbe1997b9
tazeek <tazeek.rakib@gmail.com>
2016-10-25 02:09:08 +0800

    Server modification halfway there

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
+
+@app.route('/test')
+def test():
+	return render_template('index.html')
 
 @app.route('/')
 def hello_world():
 	return 'Hello World'
 	
 if __name__ == '__main__':
-	app.run(host='127.0.0.1', port=6000, debug=True)
+	app.run(host='127.0.0.1', port=5000, debug=True)
